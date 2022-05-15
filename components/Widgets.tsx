78906b091ef54da2d008eff9c036e2112c6c439f
Peter Mbugua <petrmbugua@yahoo.com>
2022-05-15 16:07:13 +0300

    styling layout

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -4,7 +4,7 @@ import { TwitterTimelineEmbed } from 'react-twitter-embed';
 
 function Widgets() {
     return (
-        <div className='px-2 mt-2'>
+        <div className='px-2 mt-2 col-span-2 hidden lg:inline'>
             {/* Search */}
             <div className='flex items-center space-x-2 bg-gray-100 p-3 rounded-full mt-2'>
                 <SearchIcon className='h-5 w-5 text-gray-400' />
@@ -13,7 +13,7 @@ function Widgets() {
 
             <TwitterTimelineEmbed
                 sourceType="profile"
-                screenName="saurabhnemade"
+                screenName="sonnysangha"
                 options={{ height: 1000 }}
             />
         </div>
