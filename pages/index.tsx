387d107b16ef18b581194e7ffe324e8e111cddcd
Peter Mbugua <petrmbugua@yahoo.com>
2022-05-15 19:13:03 +0300

    display tweets on frontend

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,15 @@ import Head from 'next/head'
 import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
 import Widgets from '../components/Widgets'
+import { Tweet } from '../typings'
+import { fetchTweets } from '../utils/fetchTweets'
 
-const Home: NextPage = () => {
+interface Props {
+  tweets: Tweet[]
+}
+
+const Home = ({ tweets }: Props) => {
+  // console.log(tweets)
   return (
     <div className="lg:max-w-6xl mx-auto max-h-screen overflow-hidden">
       <Head>
@@ -16,7 +23,7 @@ const Home: NextPage = () => {
         <Sidebar />
 
         {/* Feed */}
-        <Feed />
+        <Feed tweets={tweets} />
         {/* Widgets */}
         <Widgets />
       </main>
@@ -29,8 +36,8 @@ export default Home
 
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-
+  const tweets = await fetchTweets()
   return {
-    props: {}, 
+    props: { tweets },
   }
 }
