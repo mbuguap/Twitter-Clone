50f7eb5ea4d804372d22ba9f4aae16b9c72953d9
Peter Mbugua <petrmbugua@yahoo.com>
2022-05-15 17:54:51 +0300

    fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
@@ -26,3 +26,11 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+
+  return {
+    props: {}, 
+  }
+}
