1faa644fe75330e8d864ae176a3129070970a343
Peter Mbugua <petrmbugua@yahoo.com>
2022-05-16 14:04:52 +0300

    get comments

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,66 +1,108 @@
-import React from 'react'
-import { Tweet } from '../typings'
+import React, { useEffect, useState } from 'react'
+import { Comment, Tweet } from '../typings'
 import TimeAgo from 'react-timeago'
 import {
-    ChatAlt2Icon,
-    HeartIcon,
-    SwitchHorizontalIcon,
-    UploadIcon,
+  ChatAlt2Icon,
+  HeartIcon,
+  SwitchHorizontalIcon,
+  UploadIcon,
 } from '@heroicons/react/outline'
+import { fetchComments } from '../utils/fetchComments'
 
 interface Props {
-    tweet: Tweet
+  tweet: Tweet
 }
 
 function Tweet({ tweet }: Props) {
-    return (
-        <div className="flex flex-col space-x-3 border-y border-gray-100 p-5">
-            <div className="flex space-x-3">
-                <img
-                    className="h-10 w-10 rounded-full object-cover"
-                    src={tweet.profileImg}
-                    alt=""
-                />
-                <div>
-                    <div className="flex items-center space-x-1">
-                        <p className="mr-1 font-bold">{tweet.username}</p>
-                        <p className="hidden text-sm text-gray-500 sm:inline">
-                            @{tweet.username.replace(/\s+/g, '').toLowerCase()} .
-                        </p>
-                        <TimeAgo
-                            className="text-sm text-gray-500"
-                            date={tweet._createdAt}
-                        />
-                    </div>
+  const [comments, setComments] = useState<Comment[]>([])
 
-                    <p className="pt-1">{tweet.text}</p>
-                    {tweet.image && (
-                        <img
-                            className="m-5 ml-0 mb-1 max-h-60 rounded-lg object-cover shadow-sm"
-                            src={tweet.image}
-                            alt=""
-                        />
-                    )}
-                </div>
-            </div>
+  const refreshComments = async () => {
+    const comments: Comment[] = await fetchComments(tweet._id)
+    setComments(comments)
+  }
 
-            <div className='mt-5 flex justify-between'>
-                <div className='flex cursor-pointer items-center space-x-3 text-gray-400'>
-                    <ChatAlt2Icon className="h-5 w-5" />
-                    <p>5</p>
-                </div>
-                <div className='flex cursor-pointer items-center space-x-3 text-gray-400'>
-                    <SwitchHorizontalIcon className="h-5 w-5" />
-                </div>
-                <div className='flex cursor-pointer items-center space-x-3 text-gray-400'>
-                    <HeartIcon className="h-5 w-5" />
-                </div>
-                <div className='flex cursor-pointer items-center space-x-3 text-gray-400'>
-                    <UploadIcon className="h-5 w-5" />
-                </div>
-            </div>
+  useEffect(() => {
+    refreshComments()
+  }, [])
+
+  // console.log(comments)
+  return (
+    <div className="flex flex-col space-x-3 border-y border-gray-100 p-5">
+      <div className="flex space-x-3">
+        <img
+          className="h-10 w-10 rounded-full object-cover"
+          src={tweet.profileImg}
+          alt=""
+        />
+        <div>
+          <div className="flex items-center space-x-1">
+            <p className="mr-1 font-bold">{tweet.username}</p>
+            <p className="hidden text-sm text-gray-500 sm:inline">
+              @{tweet.username.replace(/\s+/g, '').toLowerCase()} .
+            </p>
+            <TimeAgo
+              className="text-sm text-gray-500"
+              date={tweet._createdAt}
+            />
+          </div>
+
+          <p className="pt-1">{tweet.text}</p>
+          {tweet.image && (
+            <img
+              className="m-5 ml-0 mb-1 max-h-60 rounded-lg object-cover shadow-sm"
+              src={tweet.image}
+              alt=""
+            />
+          )}
         </div>
-    )
+      </div>
+
+      <div className="mt-5 flex justify-between">
+        <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+          <ChatAlt2Icon className="h-5 w-5" />
+          <p>{comments.length}</p>
+        </div>
+        <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+          <SwitchHorizontalIcon className="h-5 w-5" />
+        </div>
+        <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+          <HeartIcon className="h-5 w-5" />
+        </div>
+        <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+          <UploadIcon className="h-5 w-5" />
+        </div>
+      </div>
+      {/* Comment Box logic*/}
+
+      {comments?.length > 0 && (
+        <div className="my-2 mt-5 max-h-44 space-y-5 overflow-y-scroll border-t border-gray-100 p-5">
+          {comments.map((comment) => (
+            <div key={comment._id} className="relative flex space-x-2">
+                <hr className='absolute left-5 top-10 h-8 border-x border-twitter/30' />
+              <img
+                className="h-7 w-7 mt-2 rounded-full object-cover"
+                src={comment.profileImg}
+                alt=""
+              />
+              <div>
+                <div className="flex items-center space-x-1">
+                  <p className="mr-1 font-bold">{comment.username}</p>
+                  <p className="hidden text-sm text-gray-500 lg:inline">
+                    {comment.username.replace(/\s+/g, '').toLowerCase()}
+                  </p>
+                  <TimeAgo
+                    className="text-sm text-gray-500"
+                    date={comment._createdAt}
+                  />
+                </div>
+                <p> {comment.comment} </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default Tweet
