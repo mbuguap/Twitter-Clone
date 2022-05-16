b2a166a48abf634685049ab53a98daa9efa9b4bc
Peter Mbugua <petrmbugua@yahoo.com>
2022-05-16 14:51:53 +0300

    next auth implementation

--- components/SidebarRow.tsx
+++ components/SidebarRow.tsx
@@ -1,18 +1,23 @@
 import React, { SVGProps } from 'react'
 
 interface Props {
-    Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element
-    title: string
+  Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element
+  title: string
+  onClick?: () => {}
 }
 
-function SidebarRow({ Icon, title }: Props) {
-    return (
-        <div className='flex max-w-fit items-center space-x-2 px-4 py-3 rounded-full hover:bg-gray-100 cursor-pointer transition-all duration-200 group'>
-            <Icon className='h-6 w-6 ' />
-            <p className='group-hover:text-twitter hidden md:inline-flex text-base font-light lg:text-xl'>{title}</p>
-
-        </div>
-    )
+function SidebarRow({ Icon, title, onClick }: Props) {
+  return (
+    <div
+      onClick={() => onClick?.()}
+      className="group flex max-w-fit cursor-pointer items-center space-x-2 rounded-full px-4 py-3 transition-all duration-200 hover:bg-gray-100"
+    >
+      <Icon className="h-6 w-6 " />
+      <p className="hidden text-base font-light group-hover:text-twitter md:inline-flex lg:text-xl">
+        {title}
+      </p>
+    </div>
+  )
 }
 
 export default SidebarRow
--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -1,49 +1,54 @@
 import React, { useState } from 'react'
 import {
-    CalendarIcon,
-    EmojiHappyIcon,
-    LocationMarkerIcon,
-    PhotographIcon,
-    SearchCircleIcon,
+  CalendarIcon,
+  EmojiHappyIcon,
+  LocationMarkerIcon,
+  PhotographIcon,
+  SearchCircleIcon,
 } from '@heroicons/react/outline'
+import { useSession } from 'next-auth/react'
 
 function TweetBox() {
-    const [input, setInput] = useState<string>('')
+  const [input, setInput] = useState<string>('')
+  const { data: session } = useSession()
 
-    return (
-        <div className="flex space-x-2 p-5">
-            <img
-                className="mt-4 h-14 w-14 rounded-full object-cover"
-                src="https://links.papareact.com/gll"
-                alt=""
-            />
+  return (
+    <div className="flex space-x-2 p-5">
+      <img
+        className="mt-4 h-14 w-14 rounded-full object-cover"
+        src={session?.user?.image || 'https://links.papareact.com/gll'}
+        alt=""
+      />
 
-            <div className="flex flex-1 items-center pl-2">
-                <form className="flex flex-1 flex-col">
-                    <input
-                        value={input}
-                        onChange={(e) => setInput(e.target.value)}
-                        type="text"
-                        placeholder="What's Happening?"
-                        className="h-24 w-full text-xl outline-none placeholder:text-xl"
-                    />
-                    <div className="flex items-center">
-                        <div className="flex flex-1 space-x-2 text-twitter">
-                            {/* Icons */}
-                            <PhotographIcon className="transition-tra h-5 w-5 cursor-pointer duration-150 ease-out hover:scale-150" />
-                            <SearchCircleIcon className="h-5 w-5" />
-                            <EmojiHappyIcon className="h-5 w-5" />
-                            <CalendarIcon className="h-5 w-5" />
-                            <LocationMarkerIcon className="h-5 w-5" />
-                        </div>
-                        <button disabled={!input} className="rounded-full bg-twitter px-5 py-2 font-bold text-white disabled:opacity-40">
-                            Tweet
-                        </button>
-                    </div>
-                </form>
+      <div className="flex flex-1 items-center pl-2">
+        <form className="flex flex-1 flex-col">
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            placeholder="What's Happening?"
+            className="h-24 w-full text-xl outline-none placeholder:text-xl"
+          />
+          <div className="flex items-center">
+            <div className="flex flex-1 space-x-2 text-twitter">
+              {/* Icons */}
+              <PhotographIcon className="transition-tra h-5 w-5 cursor-pointer duration-150 ease-out hover:scale-150" />
+              <SearchCircleIcon className="h-5 w-5" />
+              <EmojiHappyIcon className="h-5 w-5" />
+              <CalendarIcon className="h-5 w-5" />
+              <LocationMarkerIcon className="h-5 w-5" />
             </div>
-        </div>
-    )
+            <button
+              disabled={!input || !session}
+              className="rounded-full bg-twitter px-5 py-2 font-bold text-white disabled:opacity-40"
+            >
+              Tweet
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default TweetBox
