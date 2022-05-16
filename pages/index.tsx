ee38ae95c433ff437ebcf2aacd7b7f86f767bbb5
Peter Mbugua <petrmbugua@yahoo.com>
2022-05-16 16:15:38 +0300

    remove git ignore files

---
+++ pages/index.tsx
@@ -0,0 +1,45 @@
+import type { GetServerSideProps, NextPage } from 'next'
+import Head from 'next/head'
+import { Toaster } from 'react-hot-toast'
+import Feed from '../components/Feed'
+import Sidebar from '../components/Sidebar'
+import Widgets from '../components/Widgets'
+import { Tweet } from '../typings'
+import { fetchTweets } from '../utils/fetchTweets'
+
+interface Props {
+  tweets: Tweet[]
+}
+
+const Home = ({ tweets }: Props) => {
+  // console.log(tweets)
+  return (
+    <div className="mx-auto max-h-screen overflow-hidden lg:max-w-6xl">
+      <Head>
+        <title>Twitter Clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Toaster />
+
+      <main className="grid grid-cols-9 ">
+        {/* Sidebar */}
+        <Sidebar />
+
+        {/* Feed */}
+        <Feed tweets={tweets} />
+        {/* Widgets */}
+        <Widgets />
+      </main>
+    </div>
+  )
+}
+
+export default Home
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const tweets = await fetchTweets()
+  return {
+    props: { tweets },
+  }
+}
